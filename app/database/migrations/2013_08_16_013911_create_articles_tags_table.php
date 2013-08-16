b3d0e2d4ff1cd3e952c05530c113e02a4e0be162
Fideloper <fideloper@gmail.com>
2013-08-16 01:49:34 +0000

    added table creation migrations

---
+++ app/database/migrations/2013_08_16_013911_create_articles_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTagsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles_tags', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('article_id')->unsigned()->index();
+			$table->integer('tag_id')->unsigned()->index();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles_tags');
+	}
+
+}
