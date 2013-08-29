8dce617dd26a967a227bc2d4efc8ed7d43d276ea
Chris Fidao <fideloper@gmail.com>
2013-08-28 22:27:03 -0400

    on the way to creating an article

--- app/views/admin/article_create.blade.php
+++ app/views/admin/article_create.blade.php
@@ -1,13 +1,20 @@
 <div class="row">
+    @if( count($errors->all()) )
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    @endif
     <form action="/admin/article" method="post">
         <ul>
             <li class="field">
                 <label for="title" class="inline">Title</label>
-                <input class="text input" type="text" name="title" id="title" placeholder="Title">
+                <input class="text input" type="text" name="title" id="title" placeholder="Title" value="@if( isset($input['title']) ){{ $input['title'] }}@endif">
             </li>
             <li class="field">
                 <label for="tags" class="inline">Tags</label>
-                <input class="text input" type="text" name="tags" id="tags" placeholder="Tags">
+                <input class="text input" type="text" name="tags" id="tags" placeholder="Tags" value="@if( isset($input['tags']) ){{ $input['tags'] }}@endif">
             </li>
             <li class="field">
                 <label for="status" class="">Status</label>
@@ -19,12 +26,13 @@
             </li>
             <li class="field">
                 <label for="excerpt" class="inline">Excerpt</label>
-                <textarea class="input textarea" placeholder="Excerpt" name="excerpt" id="excerpt" rows="3"></textarea>
+                <textarea class="input textarea" placeholder="Excerpt" name="excerpt" id="excerpt" rows="3">@if( isset($input['excerpt']) ){{ $input['excerpt'] }}@endif</textarea>
             </li>
             <li class="field">
                 <label for="content" class="inline">Content</label>
-                <textarea class="input textarea" placeholder="Content" name="content" id="content" rows="5"></textarea>
+                <textarea class="input textarea" placeholder="Content" name="content" id="content" rows="5">@if( isset($input['content']) ){{ $input['content'] }}@endif</textarea>
             </li>
         </ul>
+        <div class="medium default btn"><input type="submit" value="Submit"></div>
     </form>
 </div>
