f73aeb85327eb228606697d0c6afdc9ff0410a5f
Chris Fidao <fideloper@gmail.com>
2013-09-11 20:43:58 -0400

    bootstrap setup and markdown readme

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,11 +26,10 @@ $app->redirectIfTrailingSlash();
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+    return getenv('LARA_ENV') ?: 'development';
+});
 
 /*
 |--------------------------------------------------------------------------
